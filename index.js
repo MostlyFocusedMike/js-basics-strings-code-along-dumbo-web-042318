637d5537aa7f314ffe4eca8b6c659502460ee25d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-06-03 01:07:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+// Write your code in this fil
+let currentUser = "Mike",
+  welcomeMessage = `Welcome to Flatbook, ${currentUser}!`,
+  excitedWelcomeMessage = welcomeMessage.toUpperCase();
+  shortGreeting = `Welcome, ${currentUser[0]}!`;
